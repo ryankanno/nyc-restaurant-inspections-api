a00335f3414de7a29954ccf2bcb554068d0dde04
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-10-28 07:50:53 -0400

    PEP8 compliant! :D

--- data/install.py
+++ data/install.py
@@ -2,28 +2,44 @@
 # -*- coding: utf-8 -*-
 
 
-__all__     = ['main']
-__author__  = "Ryan Kanno <ryankanno@localkinegrinds.com>"
-__url__     = ""
+__all__ = ['main']
+__author__ = "Ryan Kanno <ryankanno@localkinegrinds.com>"
+__url__ = ""
 __version__ = ""
 __license__ = ""
 
 
-import argparse, platform, logging, sys, os, time
-import csv, codecs
+import argparse
+import platform
+import logging
+import sys
+import os
+import time
+import csv
+import codecs
 
-from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import Table
 from datetime import datetime
 from zipfile import ZipFile
 
 DATA_DIRECTORY = os.path.normpath(os.path.realpath(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.join(DATA_DIRECTORY, os.path.pardir))
 
-from models import Action, Violation, Cuisine, Restaurant, Inspection
-from database import db_session, init_db
+from models import Action
+from models import Violation
+from models import Cuisine
+from models import Restaurant
+from models import Inspection
+from database import db_session
+from database import init_db
 
 
-LOG_LEVEL  = logging.DEBUG
+LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
 
 
@@ -55,7 +71,8 @@ def _get_start_end_date_tuple(row):
 def _process_action_row(row):
     start, end = _get_start_end_date_tuple(row)
 
-    action = Action(row['ACTIONCODE'].strip(), row['ACTIONDESC'].strip(), start, end)
+    action = Action(row['ACTIONCODE'].strip(),
+                    row['ACTIONDESC'].strip(), start, end)
 
     db_session.add(action)
     db_session.commit()
@@ -65,7 +82,7 @@ def _process_violation_row(row):
     start, end = _get_start_end_date_tuple(row)
     is_critical = True if row['CRITICALFLAG'] == "Y" else False
 
-    violation = Violation(row['VIOLATIONCODE'].strip(), 
+    violation = Violation(row['VIOLATIONCODE'].strip(),
                           row['VIOLATIONDESC'].strip().decode('iso-8859-1'),
                           is_critical, start, end)
 
@@ -74,21 +91,23 @@ def _process_violation_row(row):
 
 
 def _process_cuisine_row(row):
-    cuisine = Cuisine(row['CUISINECODE'].strip(), row['CODEDESC'].strip().decode('iso-8859-1'))
+    cuisine = Cuisine(row['CUISINECODE'].strip(),
+                      row['CODEDESC'].strip().decode('iso-8859-1'))
 
     db_session.add(cuisine)
     db_session.commit()
 
 
 def _process_and_return_restaurant(row):
-    unique_id=row['CAMIS'].strip()
+    unique_id = row['CAMIS'].strip()
     name = row['DBA'].strip().decode('iso-8859-1')
     building = row['BUILDING'].strip()
     street = row['STREET'].strip()
     zipcode = row['ZIPCODE'].strip()
     borough = int(row['BORO'].strip())
     phone = row['PHONE'].strip()
-    restaurant = Restaurant(unique_id, name, building, street, zipcode, borough, phone)
+    restaurant = Restaurant(unique_id, name, building,
+                            street, zipcode, borough, phone)
 
     db_session.add(restaurant)
     db_session.commit()
@@ -98,36 +117,46 @@ def _process_and_return_restaurant(row):
 
 def _process_inspection_row(row):
     if row['GRADEDATE'].strip():
-        restaurant = Restaurant.query.filter_by(unique_id=row['CAMIS'].strip()).first()
+        restaurant = Restaurant.query.\
+            filter_by(unique_id=row['CAMIS'].strip()).first()
+
         if restaurant is None:
             restaurant = _process_and_return_restaurant(row)
 
-        inspection_params = {'restaurant_id': restaurant.id }
+        inspection_params = {'restaurant_id': restaurant.id}
 
-        cuisine = Cuisine.query.filter_by(code=row['CUISINECODE'].strip()).first()
+        cuisine = Cuisine.query.\
+            filter_by(code=row['CUISINECODE'].strip()).first()
+
         if cuisine is not None:
             inspection_params['cuisine_id'] = cuisine.id
 
-        inspection_params['inspected_at'] = datetime.strptime(row['INSPDATE'].strip(), CSV_DATETIME_FORMAT)
+        inspection_params['inspected_at'] = datetime.\
+            strptime(row['INSPDATE'].strip(), CSV_DATETIME_FORMAT)
 
         action = Action.query.filter_by(code=row['CUISINECODE'].strip()).\
             filter(Action.end_at >= inspection_params['inspected_at']).\
             filter(inspection_params['inspected_at'] >= Action.start_at).\
             first()
+
         if action is not None:
             inspection_params['action_id'] = action.id
-            
+
         violation = Violation.query.filter_by(code=row['VIOLCODE'].strip()).\
-                filter(Violation.end_at >= inspection_params['inspected_at']).\
-                filter(inspection_params['inspected_at'] >= Violation.start_at).\
-                first()
+            filter(Violation.end_at >= inspection_params['inspected_at']).\
+            filter(inspection_params['inspected_at'] >= Violation.start_at).\
+            first()
+
         if violation is not None:
             inspection_params['violation_id'] = violation.id
 
         inspection_params['score'] = row['SCORE'].strip()
         inspection_params['current_grade'] = row['CURRENTGRADE'].strip()
-        inspection_params['graded_at'] = datetime.strptime(row['GRADEDATE'].strip(), CSV_DATETIME_FORMAT)
-        inspection_params['generated_at'] = datetime.strptime(row['RECORDDATE'].strip().split(".")[0], CSV_DATETIME_FORMAT)
+        inspection_params['graded_at'] = datetime.\
+            strptime(row['GRADEDATE'].strip(), CSV_DATETIME_FORMAT)
+        inspection_params['generated_at'] = datetime.\
+            strptime(row['RECORDDATE'].strip().split(".")[0],
+                     CSV_DATETIME_FORMAT)
 
         inspection = Inspection(**inspection_params)
 
@@ -144,12 +173,13 @@ def init_workspace():
     if (os.path.exists('/tmp/test.db')):
         logging.debug("Removing previous database")
         os.remove('/tmp/test.db')
-    
+
 
 def cleanup():
     logging.debug("Cleaning up")
-    [os.remove(os.path.join(DATA_DIRECTORY, f)) for f in os.listdir(DATA_DIRECTORY) \
-        if f.endswith(".txt") or f.endswith(".xls")] 
+    [os.remove(os.path.join(DATA_DIRECTORY, f))
+        for f in os.listdir(DATA_DIRECTORY)
+        if f.endswith(".txt") or f.endswith(".xls")]
 
 
 def main(argv=None):
