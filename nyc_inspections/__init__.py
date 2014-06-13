3488bd3c4b77d468528631a86d3e71d7e83750e1
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-13 01:04:14 -0400

    In preparation for upgrade

---
+++ nyc_inspections/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from collections import namedtuple
+version_info = namedtuple('version_info', ('major', 'minor', 'patch'))
+
+
+VERSION = version_info(0, 0, 2)
+
+
+__title__ = 'py_skeleon'
+__version__ = '{0.major}.{0.minor}.{0.patch}'.format(VERSION)
+__author__ = 'Ryan Kanno'
+__email__ = 'ryankanno@localkinegrinds.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Ryan Kanno'
+
+# vim: filetype=python
---
+++ setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+]
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+# Metadata
+
+meta = {}
+re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
+re_version = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+strip_quotes = lambda s: s.strip("\"'")
+
+
+def add_version(match):
+    return {'VERSION': match.group(1).replace(" ", "").replace(",", ".")}
+
+
+def add_meta(match):
+    attr_name, attr_value = m.groups()
+    return {attr_name: strip_quotes(attr_value)}
+
+
+patterns = {
+    re_meta: add_meta,
+    re_version: add_version
+}
+
+
+with open(os.path.join(here, 'nyc_inspections/__init__.py'), 'r') as f:
+    for line in f:
+        for pattern, handler in patterns.items():
+            m = pattern.match(line.strip())
+            if m:
+                meta.update(handler(m))
+
+# Requires
+
+requires = ['Flask==0.9',
+            'SQLAlchemy==0.7.8',
+            'Flask-SQLAlchemy==0.16',
+            'MySQL-Python==1.2.3',
+            'fabric']
+
+tests_require = ['flake8', 'mock', 'nose', 'nosexcover']
+
+with open(os.path.join(here, 'README.rst')) as f:
+    readme = f.read()
+
+with open(os.path.join(here, 'CHANGES')) as f:
+    changes = f.read()
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Utilities'
+]
+
+setup(
+    name='nyc-inspections',
+    version=meta['VERSION'],
+    description='API for NYC restaurant inspections',
+    long_description=readme + '\n\n' + changes,
+    author=meta['author'],
+    author_email=meta['email'],
+    url="https://github.com/ryankanno/py-skeleton",
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'py_skeleton': 'py_skeleton'},
+    install_requires=requires,
+    license=meta['license'],
+    tests_require=tests_require,
+    classifiers=classifiers,
+    test_suite='nose.collector'
+)
+
+
+# vim: filetype=python
