eba97bd069b178e1dd106145e4c1c540736be00a
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-27 07:06:54 -0400

    Added Violation tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,7 @@ import datetime
 from nose.tools import ok_
 from nyc_inspections.models import BOROUGHS
 from nyc_inspections.models import Action
+from nyc_inspections.models import Violation
 import unittest
 
 
@@ -34,6 +35,25 @@ class TestModels(unittest.TestCase):
         ok_(action.description == action_desc)
         ok_(action.start_at == action_start)
         ok_(action.end_at == action_end)
+
+    def test_violation(self):
+        violation_code = "10E"
+        violation_desc = "Bad violation"
+        violation_is_critical = False
+        violation_start = datetime.datetime.utcnow()
+        violation_end = datetime.datetime.utcnow()
+        violation_params = {
+            'code': violation_code,
+            'description': violation_desc,
+            'start_at': violation_start,
+            'end_at': violation_end
+        }
+        violation = Violation(**violation_params)
+        ok_(violation.code == violation_code)
+        ok_(violation.description == violation_desc)
+        ok_(violation.is_critical == violation_is_critical)
+        ok_(violation.start_at == violation_start)
+        ok_(violation.end_at == violation_end)
 
 
 # vim: filetype=python
